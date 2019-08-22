c40459bcdbc582be391d5db104c13b0737027d13
Yuhei Okazaki <okazaki@fusic.co.jp>
2019-08-23 08:34:58 +0900

    bundle installからモデルの作成まで

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |i|
+  Task.create(
+    title: "task No.#{i}",
+    description: "This is task No.#{i}",
+    completed: false
+  )
+end
